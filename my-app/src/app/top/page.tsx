463b6b5aad8cf58c56d77c2d32d5956402fa433e
k4nkan <kanta.y.0126@gmail.com>
2024-10-24 14:51:46 +0900

    #5 ファイル構造の調整

--- my-app/src/app/top/page.tsx
+++ my-app/src/app/top/page.tsx
@@ -3,10 +3,16 @@ import Link from 'next/dist/client/link';
 export default function Home() {
   return (
     <div>
-      <div>this is connect page</div>
+      <div>this is top page</div>
       <ul>
         <li>
           <Link href="/top">Top Page</Link>
+        </li>
+        <li>
+          <Link href="/about">About</Link>
+        </li>
+        <li>
+          <Link href="/connect">Connect</Link>
         </li>
       </ul>
     </div>
